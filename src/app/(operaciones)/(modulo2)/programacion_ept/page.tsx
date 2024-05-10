a107c712826b476ce1bfbc4517d3bbb10fdd9f59
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2024-05-09 23:58:49 -0500

    modulo 2 inicio

---
+++ src/app/(operaciones)/(modulo2)/programacion_ept/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+
+import React, { useState } from 'react';
+import Sidebar from '@/components/sideBar';
+import UserBanner from '@/components/BannerUsuario';
+import { usePathname } from 'next/navigation';
+
+
+
+
+export default function  Panel() {
+  const pathname = usePathname();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+
+  
+  return (
+    <div className="flex flex-col min-h-screen bg-white relative">
+    
+      <UserBanner userProfilePic={'userCat.jpg'} pageName={''}/>
+      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} activePage={pathname} />
+      {sidebarOpen && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: 100, 
+          }}
+          onClick={() => setSidebarOpen(false)} 
+        />
+      )}
+      <div className={sidebarOpen ? "content-overlay" : ""}>
+
+      <p className="pl-20 text-black mt-4 text-xl">
+            HOLA <span className="font-bold">PROGRAMACION EPT</span> 
+      </p>
+      </div>
+    </div>
+  );
+}
